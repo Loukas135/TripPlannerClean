2b3319b3f4e0e3cfe44cf363152489310928fd04
waitwhat2231 <somethingsomeone036+12@gmail.com>
2024-05-31 13:40:59 +0300

    whsss

--- TripPlanner.API/Extensions/WebApplicationBuilderExtension.cs
+++ TripPlanner.API/Extensions/WebApplicationBuilderExtension.cs
@@ -56,13 +56,7 @@ namespace TripPlanner.API.Extensions
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    b => b.AllowAnyHeader()
-                        .AllowAnyOrigin()
-                        .AllowAnyMethod());
-            });
+            
 
             builder.Services.AddIdentityApiEndpoints<User>();
 		}
