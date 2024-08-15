f332b0befed75eaf5453cf8ebc6394880862d4be
waitwhat2231 <somethingsomeone036+12@gmail.com>
2024-08-15 23:10:45 +0300

    fixed dto

--- TripPlanner.Application/Rooms/Dtos/RoomDto.cs
+++ TripPlanner.Application/Rooms/Dtos/RoomDto.cs
@@ -11,11 +11,12 @@ namespace TripPlanner.Application.Rooms.Dtos
 	public class RoomDto
 	{
 		public int Id { get; set; }
+		public string? Title { get; set; }
 		public string Description { get; set; } = default!;
 		public int NumberOfPeople { get; set; }
 		public int Quantity { get; set; }
 		public float PricePerNight { get; set; }
-		public string ImagePath { get; set; }
+		public string? ImagePath { get; set; }
 		public List<ReservationDto>? Reservations { get; set; }
 		public int RoomCategoryId { get; set; }
 	}
