78d60eb4d45afe5c90de1f64fe92e23a754404be
waitwhat2231 <somethingsomeone036+12@gmail.com>
2024-08-16 22:56:53 +0300

    bruh edit

--- TripPlanner.Application/Services/Dtos/ServiceDto.cs
+++ TripPlanner.Application/Services/Dtos/ServiceDto.cs
@@ -17,7 +17,8 @@ namespace TripPlanner.Application.Services.Dtos
 	{
 		public int Id { get; set; }
 		public string Name { get; set; } = default!;
-		public string Address { get; set; } = default!;
+        public int GovernorateId { get; set; }
+        public string Address { get; set; } = default!;
 		public string Description { get; set; } = default!;
 		public string ContactNumber { get; set; } = default!;
 		public string? ContactEmail { get; set; }
